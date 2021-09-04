ea93fec4dd94d67b50b4fa85964d67e386e95a9f
Leonkoithara <leon.koithara@gmail.com>
2021-09-04 13:38:30 +0530

    Changed to single vao

--- src/main.cpp
+++ src/main.cpp
@@ -44,53 +44,34 @@ int main()
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	unsigned int vao1 = 0, vao2 = 0;
+	unsigned int vao1 = 0;
 
 	glCreateVertexArrays(1, &vao1);
-	glCreateVertexArrays(1, &vao2);
 
 	float vbo_data[] =
 	{
-		-0.75f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.25f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-		-0.15f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
-		0.75f,   0.5f, 0.0f, 1.0f, 1.0f, 0.0f
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f,
+		 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f,		
 	};
 	unsigned int t1_buff;
 	glGenBuffers(1, &t1_buff);
 
-	// First triangle
 	glBindVertexArray(vao1);
 
 	unsigned int i1[] = {
-		0, 1, 2
+		0, 1, 2,
+		1, 2, 3
 	};
 
 	unsigned int index1;
 	glGenBuffers(1, &index1);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index1);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), i1, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), i1, GL_STATIC_DRAW);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, t1_buff);
 	glBufferData(GL_ARRAY_BUFFER, 4 * 6 * sizeof(float), vbo_data, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-
-	// Second triangle
-	glBindVertexArray(vao2);
-	unsigned int i2[] = {
-	1, 2, 3
-	};
-
-	unsigned int index2;
-	glGenBuffers(1, &index2);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index2);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 3 * sizeof(unsigned int), i2, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ARRAY_BUFFER, t1_buff);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
@@ -105,9 +86,7 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glBindVertexArray(vao1);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);
-		glBindVertexArray(vao2);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, NULL);		
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 
 		glfwSwapBuffers(window);
 
